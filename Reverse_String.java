935e107eecd97561da62dad730df349a461908e9
Dharshini Ravikumar <dharshinimm2004@gmail.com>
2025-01-16 12:44:09 +0530

    Add files via upload

---
+++ Reverse_String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Reverse_String{
+    public static void main(String[] args){
+        String given="Dharshini";
+        StringBuffer string = new StringBuffer();
+        string.append(given);
+        System.out.println(string.reverse());
+    }
+}
