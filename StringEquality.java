84be0f6ef4f2da24555ccec4835b23c6936aa4a8
Dharshini Ravikumar <dharshinimm2004@gmail.com>
2025-01-24 08:00:58 +0530

    Add files via upload

---
+++ StringEquality.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class StringEquality{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a="apple";
+        String b="apple";
+        System.out.println("The both strings are equal "+(a==b));
+    }
+}
